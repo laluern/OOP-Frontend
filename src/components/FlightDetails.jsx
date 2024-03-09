42cb8415ba251ac94dc782aaeecdb6f8adcb232f
MustMark <Superpeerawat.mm@gmail.com>
2024-03-09 23:20:37 +0700

    update Select Flight Page

--- src/components/FlightDetails.jsx
+++ src/components/FlightDetails.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
-function FlightDetails({flightInfo}){
-    return <header className="FlightDetails">
-        <h1>{flightInfo}</h1>
-        </header>
+function FlightDetails({flight_info}){
+    return   <div>
+    <p>Departure: {flight_info.departure}</p>
+    <p>Departure Time: {flight_info.departure_time}</p>
+    <p>Destination: {flight_info.destination}</p>
+    <p>destination Time : {flight_info.destinationtime}</p>
+    <p>Duration: {flight_info.duration}</p>
+    <p>Price: {flight_info.price}</p>
+    <p>Discount: {flight_info.discount}</p>
+  </div>
     
 }
 
--- src/pages/SearchResult.jsx
+++ src/pages/SearchResult.jsx
@@ -1,21 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Nav from '../components/Nav';
 import { useSearchParams, useLocation } from 'react-router-dom';
+import FlightDetails from '../components/FlightDetails';
 
 function SearchResult() {
   const location = useLocation()
 
-  console.log(JSON)
-  const [searchParams, setSearchParams] = useSearchParams();
-  searchParams.get("departure")
-  searchParams.get("destination")
+  // console.log(JSON)
+  // const [searchParams, setSearchParams] = useSearchParams();
+  // searchParams.get("departure")
+  // searchParams.get("destination")
+
+  const [flightList] = useState(location.state.flightResult);
+
   return (
     <div>
-      <Nav/>
-      <h1>{JSON.stringify(location.state.flightResult)}</h1>
+      <Nav />
+      {Object.keys(flightList).map(flightKey => (
+        <div key={flightKey}>
+          <h1>Flight Key: {flightKey}</h1>
+          <FlightDetails flight_info={flightList[flightKey]} />
+          <button>Select Flight</button>
+          <h2>**************************************</h2>
+        </div>
+      ))}
     </div>
-
-  )
+  );
 }
 
 export default SearchResult
