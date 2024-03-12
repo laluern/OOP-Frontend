3c398afa80923bf8cd6ace2d0b2a1ccffd9a24f1
LTRLF <issareeniss@gmail.com>
2024-03-12 15:24:30 +0700

    class to className

--- src/components/PassengerInfo.jsx
+++ src/components/PassengerInfo.jsx
@@ -46,7 +46,7 @@ function PassengersInfo(){
     return (
     <div>
         
-        <div classNameName="grid md:grid-cols-2 md:gap-6">
+        <div className="grid md:grid-cols-2 md:gap-6">
             <div className="relative z-0 w-full mb-5 group">
                 <input type="text" placeholder="" value={firstname} onChange={handleFirstnameChange} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" required/>
                 <label for="floating_first_name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">First name</label>
